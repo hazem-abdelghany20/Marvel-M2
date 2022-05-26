9ee414af94b6add59eab9f4c27fc9046bf5123cc
hazzouma339 <105674844+hazzouma339@users.noreply.github.com>
2022-05-26 13:30:20 +0200

    update

--- src/Control/Gui.java
+++ src/Control/Gui.java
@@ -1,11 +1,36 @@
 package Control;
 
 import engine.*;
+import model.abilities.Ability;
+import model.abilities.AreaOfEffect;
+import model.abilities.CrowdControlAbility;
+import model.abilities.DamagingAbility;
+import model.abilities.HealingAbility;
+import model.effects.Disarm;
+import model.effects.Dodge;
+import model.effects.Effect;
+import model.effects.Embrace;
+import model.effects.PowerUp;
+import model.effects.Root;
+import model.effects.Shield;
+import model.effects.Shock;
+import model.effects.Silence;
+import model.effects.SpeedUp;
+import model.effects.Stun;
+import model.world.AntiHero;
 import model.world.Champion;
+import model.world.Hero;
+import model.world.Villain;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -30,20 +55,70 @@ public class Gui extends JFrame {
 	private JTextField Pl1,Pl2;
 	private Player Player1;
 	private Player Player2;
+	private JButton bCap; private JButton bDeadpool; private JButton bDrStrange; private JButton bElectro;
+	private JButton bGhostRider; private JButton bHela; private JButton bHulk; private JButton bIceman;
+	private JButton bIronman; private JButton bLoki; private JButton bQuickSilver; private JButton bSpiderman;
+	private JButton bThor; private JButton bVenom; private JButton bYellowJacket;
 	
-	public Gui() {
+	public Gui() throws IOException {
 		sw = new StartingWindow();
 		pw = new PlayersWindow();
-		w= new Window();
 		cw= new ChampionsWindow();
 		
 		
-			Play = new JButton("Play");
-			Play.setBounds(350, 500, 100, 60);
-			Play.setForeground(Color.white);
-	    	Play.setBackground(Color.red);
+		Play = new JButton("Play");
+		Play.setSize(100, 60);
+		Play.setForeground(Color.white);
+	    Play.setBackground(Color.red);
 	    
-	    	sw.add(Play);
+	    sw.add(Play,BorderLayout.CENTER);
+	    
+	    bCap= new JButton(new ImageIcon(this.getClass().getResource("/image/captain.jpeg"))); 
+	    bDeadpool= new JButton(new ImageIcon(this.getClass().getResource("/image/deadpool.jpeg")));
+	    bDrStrange=new JButton(new ImageIcon(this.getClass().getResource("/image/drstrange.jpeg")));
+	    bElectro=new JButton(new ImageIcon(this.getClass().getResource("/image/electro.jpeg")));
+	    bGhostRider=new JButton(new ImageIcon(this.getClass().getResource("/image/ghostrider.jpeg")));
+	    bHela=new JButton(new ImageIcon(this.getClass().getResource("/image/hela.jpeg")));
+	    bHulk=new JButton(new ImageIcon(this.getClass().getResource("/image/hulk.jpeg")));
+	    bIceman=new JButton(new ImageIcon(this.getClass().getResource("/image/iceman.jpeg")));
+	    bIronman=new JButton(new ImageIcon(this.getClass().getResource("/image/ironman.jpeg")));
+	    bLoki=new JButton(new ImageIcon(this.getClass().getResource("/image/loki.jpeg")));
+	    bQuickSilver=new JButton(new ImageIcon(this.getClass().getResource("/image/quicksilver.jpeg")));
+	    bSpiderman=new JButton(new ImageIcon(this.getClass().getResource("/image/spiderman.jpeg")));
+	    bThor=new JButton(new ImageIcon(this.getClass().getResource("/image/thor.jpeg")));
+	    bVenom=new JButton(new ImageIcon(this.getClass().getResource("/image/venom.jpeg")));
+	    bYellowJacket=new JButton(new ImageIcon(this.getClass().getResource("/image/yellowjacket.jpeg")));
+	
+	
+		Game.loadAbilities("Abilities.csv");
+		Game.loadChampions("Champions.csv");
+		int i=0;
+		while(i<Game.getAvailableChampions().size()) {
+			String name = Game.getAvailableChampions().get(i).getName();
+			JButton b=null;
+			switch(name){
+			case"Captain America": b= bCap; break;
+			case"Deadpool": b= bDeadpool; break;
+			case"Dr Strange":  b= bDrStrange; break;
+			case"Electro":  b= bElectro; break;
+			case"Ghost Rider":  b= bGhostRider; break;
+			case"Hela":  b= bHela; break;
+			case"Hulk":  b= bHulk; break;
+			case"Iceman": b= bIceman; break;
+			case"Ironman": b= bIronman; break;
+			case"Loki": b= bLoki; break;
+			case"Quicksilver": b= bQuickSilver; break;
+			case"Spiderman": b= bSpiderman; break;
+			case"Thor": b= bThor; break;
+			case"Venom": b= bVenom; break;
+			case"Yellow Jacket": b= bYellowJacket; break;
+			default: break;
+			}
+			b.setSize(150,150);
+			cw.getChamps().add(b,i);
+			i++;
+		}
+		
 			Play.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					sw.setVisible(false);
@@ -52,11 +127,14 @@ public class Gui extends JFrame {
 		sw.revalidate();
 		sw.repaint();
 		
-		Enter= new JButton ("Enter");
-		Enter.setBounds(310,480,100,100); 
-		Pl1= new JTextField ("name");
+		JButton Enter = new JButton("Enter");
+		Enter.setBounds(320, 400, 100, 60);
+		Enter.setForeground(Color.white);
+		Enter.setBackground(Color.red);
+		
+		Pl1= new JTextField ("Player 1");
 		Pl1.setBounds(100,350,100,30);
-		Pl2= new JTextField ("name");
+		Pl2= new JTextField ("Player 2");
 		Pl2.setBounds(550,350,100,30);
 		
 		pw.add(Enter);pw.add(Pl1);pw.add(Pl2);
@@ -65,8 +143,8 @@ public class Gui extends JFrame {
 		
 		Enter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(Pl1.getText()==null||Pl2.getText()==null) {return;
-				}else {
+				if(Pl1.getText()==null||Pl2.getText()==null) {return;}
+				else {
 				String P1name = Pl1.getText();
 				String P2name = Pl2.getText();
 				Player1 = new Player(P1name);
@@ -74,23 +152,160 @@ public class Gui extends JFrame {
 				pw.setVisible(false);
 				cw.setVisible(true);;} }});
 		
-		
 		cw.revalidate();
 		cw.repaint();
-		//game.loadChampions(Champions.csv);
-		for(Champion c: game.getAvailableChampions() ) {
-			
-			JButton b= new JButton ();
-			b.setText(getName());
-			cw.getChamps().add(b);
-		}
+		
+		cw.getChamps().setPreferredSize (new Dimension (750, 500));
+		cw.getMain().add(cw.getChamps());
 		
 		
-	}
-	public static void main (String [] args) {
-		new Gui();
+		bCap.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bCap) {
+					bCap.setEnabled(false);
+				}}
+		});
+		bDeadpool.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bDeadpool) {
+					bDeadpool.setEnabled(false);
+				}}
+		});
+		bDrStrange.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bDrStrange) {
+					bDrStrange.setEnabled(false);
+				}}
+		});
+		bElectro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bElectro) {
+					bElectro.setEnabled(false);
+				}}
+		});
+		bGhostRider.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bGhostRider) {
+					bGhostRider.setEnabled(false);
+				}}
+		});
+		bHela.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bHela) {
+					bHela.setEnabled(false);
+				}}
+		});
+		bHulk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bHulk) {
+					bHulk.setEnabled(false);
+				}}
+		});
+		bIceman.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bIceman) {
+					bIceman.setEnabled(false);
+				}}
+		});
+		bIronman.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bIronman) {
+					bIronman.setEnabled(false);
+				}}
+		});
+		bLoki.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bLoki) {
+					bLoki.setEnabled(false);
+				}}
+		});
+		bQuickSilver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bQuickSilver) {
+					bQuickSilver.setEnabled(false);
+				}}
+		});
+		bSpiderman.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bSpiderman) {
+					bSpiderman.setEnabled(false);
+					
+				}}
+		});
+		bThor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bThor) {
+					bThor.setEnabled(false);
+				}}
+		});
+		bVenom.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bVenom) {
+					bVenom.setEnabled(false);
+				}}
+		});
+		bYellowJacket.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==bYellowJacket) {
+					bYellowJacket.setEnabled(false);
+				}}
+		});
+		
+		
+		
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==bCap) {
+			bCap.setEnabled(false);
+		}
+		if(e.getSource()==bDeadpool) {
+			bDeadpool.setEnabled(false);
+		}
+		if(e.getSource()==bDrStrange) {
+			bDrStrange.setEnabled(false);
+		}
+		if(e.getSource()==bElectro) {
+			bElectro.setEnabled(false);
+		}
+		if(e.getSource()==bGhostRider) {
+			bGhostRider.setEnabled(false);
+		}
+		if(e.getSource()==bHela) {
+			bHela.setEnabled(false);
+		}
+		if(e.getSource()==bHulk) {
+			bHulk.setEnabled(false);
+		}
+		if(e.getSource()==bIceman) {
+			bIceman.setEnabled(false);
+		}
+		if(e.getSource()==bIronman) {
+			bIronman.setEnabled(false);
+		}
+		if(e.getSource()==bLoki) {
+			bLoki.setEnabled(false);
+		}
+		if(e.getSource()==bQuickSilver) {
+			bQuickSilver.setEnabled(false);
+		}
+		if(e.getSource()==bSpiderman) {
+			bSpiderman.setEnabled(false);
+		}
+		if(e.getSource()==bThor) {
+			bThor.setEnabled(false);
+		}
+		if(e.getSource()==bVenom) {
+			bVenom.setEnabled(false);
+		}
+		if(e.getSource()==bYellowJacket) {
+			bYellowJacket.setEnabled(false);
+		}
+	}
+	
+	public static void main (String [] args) throws IOException {
+		new Gui();
+	}
 	
 	
 }
--- src/View/ChampionsWindow.java
+++ src/View/ChampionsWindow.java
@@ -1,32 +1,62 @@
 package View;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.*;
 
 public class ChampionsWindow extends JFrame {
 
-	 JLabel select;
-	 JPanel Champs;
+	 private JLabel select;
+	 private JPanel Champs;
+	 private JPanel instructions; 
+	 private JPanel main;
 	 
 	public ChampionsWindow() {
 		
-		JLabel select = new JLabel("Choose your champions");
-		select.setBounds(300, 100, 200, 30);
+		JLabel select = new JLabel("Player 1, Choose your champions!");
+		select.setBounds(100, 100, 200, 100);
+		select.setFont(new Font("Serif", Font.BOLD, 30));
+		
+		main = new JPanel();
+		
+		instructions = new JPanel();
+		instructions.setBorder(BorderFactory.createEmptyBorder(50,50,50,50));
+		add(instructions, BorderLayout.NORTH);
+		//Champs.setBounds(50, 50, 50, 50);
+		instructions.setSize(new Dimension(200,200));
+		instructions.add(select);
+		main.add(instructions);
 		
 		Champs = new JPanel();
-		Champs.setLayout(new GridLayout(0, 3));
-		add(Champs, BorderLayout.CENTER);
-		
-		
-		setLayout(null);  
-		setBounds(300, 150, 800, 522);
+		Champs.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		Champs.setLayout(new GridLayout(0, 5));
+		main.add(Champs, BorderLayout.SOUTH);
+		//Champs.setBounds(50, 50, 50, 50);
+		Champs.setSize(new Dimension(750,500));
 		setSize(800,800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		
+		add(main);
+	}
+
+	public JPanel getMain() {
+		return main;
 	}
 
 	public JPanel getChamps() {
 		return Champs;
+	}
+	public void addd(JPanel p) {
+		add(p);
+	}
+	public void addd(JButton b) {
+		Champs.add(b);
+	}
+
+	public JPanel getInstructions() {
+		return instructions;
 	}
 }
